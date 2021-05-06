c58e00055a49b06f5e0f03f918b15772a7832fee
Rebecca De Venezia <rebeccaleahdevenezia@gmail.com>
2021-05-06 19:31:59 -0400

    bug fixes-base program runs, needs equality precondition

--- tarski-test.py
+++ tarski-test.py
@@ -1,21 +1,14 @@
 import tarski
 from tarski.syntax import land
+import tarski.fstrips as fs
 
 # define the language
 lang = tarski.language('playlist', theories = ['equality'])
 
-# define sorts/types - genre is the parent of all genre types
-song = lang.sort('song')
+# define the genre type
 genre = lang.sort('genre')
-rock = lang.sort('rock', genre)
-pop = lang.sort('pop', genre)
-country = lang.sort('country', genre)
-jazz = lang.sort('jazz', genre)
-classical = lang.sort('classical', genre)
 
 # define predicates/fluents (what can be true/false)
-#testsss
-
 # is this song already in the playlist?
 in_playlist = lang.predicate('in_playlist', 'song')
 # song2 comes after song1? (used to see the final order)
@@ -26,12 +19,19 @@ has_genre = lang.predicate('has_genre', 'song', 'genre')
 empty = lang.predicate('empty')
 
 # define objects
-rock1 = lang.constant('rock1', 'song')
-pop1 = lang.constant('pop1', 'song')
-jazz1 = lang.constant('jazz1', 'song')
-classical1 = lang.constant('classical1', 'song')
-classical2 = lang.constant('classical2', 'song')
-classical3 = lang.constant('classical3', 'song')
+#songs that could be added
+rock_song = lang.constant('rock_song', 'song')
+pop_song = lang.constant('pop_song', 'song')
+jazz_song = lang.constant('jazz_song', 'song')
+classical_song = lang.constant('classical_song', 'song')
+classical_song_2 = lang.constant('classical_song_2', 'song')
+classical_song_3 = lang.constant('classical_song_3', 'song')
+#genres each song can have
+rock_genre = lang.constant('rock_genre', 'genre')
+pop_genre = lang.constant('pop_genre', 'genre')
+country_genre = lang.constant('country_genre', 'genre')
+jazz_genre = lang.constant('jazz_genre', 'genre')
+classical_genre = lang.constant('classical_genre', 'genre')
 
 # define the problem
 problem = tarski.fstrips.create_fstrips_problem(
@@ -44,36 +44,35 @@ prev = lang.variable('prev', 'song')
 genre1 = lang.variable('genre1', 'genre')
 genre2 = lang.variable('genre2', 'genre')
 
-# add song action - can add songs in any order, just has to be shuffled
+# action to add a song - can add songs in any order, just has to be shuffled
 add_song = problem.action('add song', [add, prev, genre1, genre2], precondition = not in_playlist(add) & in_playlist(prev) & 
-has_genre(add, genre1) & has_genre(prev, genre2) & (not (= genre1 genre2)),
+has_genre(add, genre1) & has_genre(prev, genre2), #& (not (= genre1 genre2)),
 effects = [
-problem.AddEffect(in_playlist(add)),
-problem.AddEffect(on(prev, add))
+fs.AddEffect(in_playlist(add)),
+fs.AddEffect(on(prev, add))
 ])
 
-# add FIRST song
-add_first_song = problem.action('add song', [add], precondition = empty,
+# action to add the FIRST song
+add_first_song = problem.action('add first song', [add], precondition = empty,
 effects = [
-problem.AddEffect(in_playlist(add)),
-problem.DelEffect(empty)
+fs.AddEffect(in_playlist(add)),
+fs.DelEffect(empty)
 ])
-
-# delete song
 
 # define the initial situation
 init = tarski.model.create(lang)
 
-init.add(in_playlist(rock1))
-init.add(has_genre(rock1, rock))
-init.add(has_genre(pop1, pop))
-init.add(has_genre(jazz1, jazz))
-init.add(has_genre(classical1, classical))
-init.add(has_genre(classical2, classical))
-init.add(has_genre(classical3, classical))
+init.add(in_playlist(rock_song))
+init.add(has_genre(rock_song, rock_genre))
+init.add(has_genre(pop_song, pop_genre))
+init.add(has_genre(jazz_song, jazz_genre))
+init.add(has_genre(classical_song, classical_genre))
+init.add(has_genre(classical_song_2, classical_genre))
+init.add(has_genre(classical_song_3, classical_genre))
+
 problem.init = init
 
 # define the goal
-problem.goal = land(in_playlist(pop1), in_playlist(jazz1), in_playlist(classical1), in_playlist(classical2), in_playlist(classical3))
+problem.goal = land(in_playlist(pop_song), in_playlist(jazz_song), in_playlist(classical_song), in_playlist(classical_song_2), in_playlist(classical_song_3))
 
 print(problem.init.as_atoms())
